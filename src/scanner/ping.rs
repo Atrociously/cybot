1bb4d43360f243c4b430d9cbaac9ee8393184163
Jack Morrison <jackamorr@gmail.com>
2023-04-12 13:12:57 -0500

    Working ping implementation

--- src/scanner/ping.rs
+++ src/scanner/ping.rs
@@ -2,7 +2,6 @@ use cortex_m::peripheral::NVIC;
 
 use crate::hal::interrupt;
 use crate::measure::Distance;
-use crate::util::CriticalCell;
 use crate::{get_cybot, bits::*, SpinTimer};
 
 #[derive(Clone, Copy)]
@@ -21,6 +20,14 @@ impl PingState {
         matches!(self, Self::Done(..))
     }
 
+    fn is_low(self) -> bool {
+        matches!(self, Self::Low)
+    }
+
+    fn is_high(self) -> bool {
+        matches!(self, Self::High)
+    }
+
     fn as_done(self) -> u32 {
         if let Self::Done(v) = self {
             v
@@ -33,7 +40,8 @@ impl PingState {
 const MICROS_PER_TICK: f32 = 0.0625;
 const SOUND_CM_PER_MICRO: f32 = 0.0343;
 
-static STATE: CriticalCell<PingState> = CriticalCell::new(PingState::Low);
+static mut STATE: PingState = PingState::Low;
+//static STATE: CriticalCell<PingState> = CriticalCell::new(PingState::Low);
 
 static mut PING: Option<Ping> = Some(Ping(()));
 
@@ -58,16 +66,22 @@ impl Ping {
             // setup gpio for digital output when we need to trigger the start pulse
             gpiob.den.modify(|r, w| unsafe { w.bits(r.bits() | BIT3) });
             gpiob.dir.modify(|r, w| unsafe { w.bits(r.bits() | BIT3) });
+            // setup gpio for timer alternate function when we need to connect the timer
+            gpiob.pctl.modify(|r, w| unsafe { w.bits(r.bits() | 0x7000) });
 
             // disable timer for setup
             timer.ctl.modify(|_, w| w.tben().clear_bit());
 
             timer.cfg.modify(|_, w| unsafe { w.bits(0x4) }); // setup timers as 16-bit
-            timer.tbmr.modify(|_, w| w.tbcmr().set_bit()); // setup timer for capture mode
-            
+            timer.tbmr.modify(|_, w| w.tbmr().cap().tbcmr().set_bit().tbcdir().clear_bit()); // setup timer for capture mode
+            timer.ctl.modify(|_, w| w.tbevent().both()); // setup timer event mode for both edges
+
+            timer.tbpr.modify(|r, w| unsafe { w.bits(r.bits() | 0xFF) });
+            timer.tbilr.modify(|r, w| unsafe { w.bits(r.bits() | 0xFFFF) });
+
             unsafe { NVIC::unmask(interrupt::TIMER3B) }; // enable interrupts for timer3
             timer.icr.write(|w| w.cbmcint().set_bit().cbecint().set_bit()); // clear interrupts
-            timer.imr.modify(|_, w| w.cbmim().clear_bit().cbeim().clear_bit()); // enable interupts
+            timer.imr.modify(|_, w| w.cbeim().set_bit()); // enable interupts
             Some(ping)
         })
     }
@@ -75,20 +89,19 @@ impl Ping {
     pub fn ping_trigger(&self) {
         let cy = get_cybot();
         cortex_m::interrupt::free(|cs| {
-            let mut state = STATE.borrow_mut(cs);
-            *state = PingState::Low;
+            unsafe { STATE = PingState::Low };
 
             let timer = cy.timer3.borrow(cs);
             let gpio = cy.gpiob.borrow(cs);
 
             timer.ctl.modify(|_, w| w.tben().clear_bit());
             timer.imr.modify(|_, w| w.cbeim().clear_bit());
-            gpio.afsel.modify(|r, w| unsafe { w.bits(r.bits() | !BIT3) });
+            gpio.afsel.modify(|r, w| unsafe { w.bits(r.bits() & !BIT3) });
 
             // trigger start pulse
             gpio.data.modify(|r, w| unsafe { w.bits(r.bits() | BIT3) });
             SpinTimer.wait_micros(2);
-            gpio.data.modify(|r, w| unsafe { w.bits(r.bits() | !BIT3)});
+            gpio.data.modify(|r, w| unsafe { w.bits(r.bits() & !BIT3)});
 
             timer.icr.write(|w| w.cbecint().set_bit()); // clear erroneous interrupt
             gpio.afsel.modify(|r, w| unsafe { w.bits(r.bits() | BIT3) }); // enable alternate fn
@@ -99,12 +112,19 @@ impl Ping {
 
     pub fn get_distance(&self) -> Distance {
         self.ping_trigger();
-        while !matches!(STATE.read(), PingState::Done(..)) {}
-        let value = STATE.read().as_done();
+        let value = {
+            while unsafe { !STATE.is_done() } { }
+            unsafe { STATE.as_done() }
+        };
         cortex_m::interrupt::free(|cs| {
-            let mut state = STATE.borrow_mut(cs);
-            *state = PingState::Low;
-            // todo reset code
+            unsafe { STATE = PingState::Low };
+
+
+            let timer = get_cybot().timer3.borrow(cs);
+            // disable timer and reset counter
+            timer.ctl.modify(|_, w| w.tben().clear_bit());
+            timer.tbpr.modify(|r, w| unsafe { w.bits(r.bits() | 0xFF) });
+            timer.tbv.modify(|r, w| unsafe { w.bits(r.bits() | 0xFFFF) });
         });
         let micros = MICROS_PER_TICK * value as f32;
         Distance::from_cm(micros * SOUND_CM_PER_MICRO)
@@ -113,11 +133,31 @@ impl Ping {
 
 #[interrupt]
 fn TIMER3B() {
+    static mut start_val: u32 = 0;
+
     let cy = get_cybot();
     cortex_m::interrupt::free(|cs| {
         let timer = cy.timer3.borrow(cs);
+        if timer.mis.read().cbemis().bit_is_set() {
+            let timer_val = {
+                let prescale = timer.tbpv.read().bits() << 15;
+                let value = timer.tbv.read().bits();
+                prescale | value
+            };
 
-        if timer.ris.read().cberis().bit_is_set() {
+            if unsafe { STATE.is_low() } {
+                *start_val = timer_val;
+                unsafe { STATE = PingState::High };
+            } else if unsafe { STATE.is_high() } {
+                let difference = *start_val - timer_val;
+                *start_val = 0;
+                unsafe { STATE = PingState::Done(difference) }
+            } else {
+                panic!("Interrupt should not happen when state is done");
+            }
+
+            // clear the interrupt
+            timer.icr.write(|w| w.cbecint().set_bit());
         }
     });
 }
